103e9dedc407f512e302af71a544c7869d4defac
Davelexic <dschirmerii@gmail.com>
2025-08-20 12:32:44 -0700

    Add book upload endpoint with markup persistence

--- backend/app/models/book.py
+++ backend/app/models/book.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from typing import Optional
 
+from sqlalchemy import Column, JSON
 from sqlmodel import Field, Relationship, SQLModel
 
 
@@ -11,5 +12,6 @@ class Book(SQLModel, table=True):
     title: str
     author: Optional[str] = None
     owner_id: Optional[int] = Field(default=None, foreign_key="user.id")
+    markup: Optional[dict] = Field(default=None, sa_column=Column(JSON))
 
     owner: Optional["User"] = Relationship(back_populates="books")
